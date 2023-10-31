a14f029285e77a1640ef99a43d38c0f162c3880d
Sunny Siddhu <119279854+sunnysiddhu21@users.noreply.github.com>
2023-10-31 22:02:03 +0530

    Time: 7 ms (89.64%) | Memory: 12.8 MB (39.93%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,17 +1,51 @@
 class Solution {
 public:
     
-    int solve(vector<int> &nums,int target,int ans,int index)
-    {
-        if(index==nums.size()&&ans==target)
-          return 1;
-        if(index>=nums.size())
-          return 0;
+    int numOfSubsets(int i, int target, vector<int>& nums, vector<vector<int>>& dp) {
+        // base case
+        if(i == 0) {
+            if(target == 0 and nums[0] == 0) {
+                return 2; // either take that 0 or not take
+            }
+            if(target == nums[0] or target == 0) {
+                return 1;
+            }
+            return 0;
+        }
 
-        return solve(nums,target,ans+nums[index],index+1)+solve (nums,target,ans-nums[index],index+1);
+        if(dp[i][target] != -1) return dp[i][target];
+
+        // recursive case
+        int notTake = numOfSubsets(i-1, target, nums, dp);
+        int take = 0;
+        if(target >= nums[i]) {
+            take = numOfSubsets(i-1, target - nums[i], nums, dp);
+        }
+
+        return dp[i][target] = take + notTake;
     }
+public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int ans=0,i=0;
-        return solve(nums,target,ans,i);
+        int n = nums.size();
+
+        int totalSum = 0;
+
+        for(int i = 0; i < n; i++) {
+            totalSum += nums[i];
+        }
+
+        int newTarget = (totalSum + target);
+
+        if(newTarget%2 != 0 or newTarget < 0) {
+            return 0;
+        }
+
+        newTarget /= 2;
+
+        if(target > totalSum) return 0;
+
+        vector<vector<int>> dp(n, vector<int>(newTarget+1, -1));
+
+        return numOfSubsets(n-1, newTarget, nums, dp);
     }
 };
