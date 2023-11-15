c75fa105a67038a13aef6ee710551ca8fc68f835
Sunny Siddhu <119279854+sunnysiddhu21@users.noreply.github.com>
2023-11-15 10:30:39 +0530

    Time: 38 ms (42.73%) | Memory: 30.3 MB (39.94%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -14,10 +14,21 @@ public:
 	return dp[i][j] = recurr(i-1, j, str, sub, dp)%mod;
     }
 
-    int numDistinct(string str, string sub) {
-        int n=str.size();
-	    int m=sub.size();
-	    vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return recurr(n-1, m-1, str, sub, dp);
+    int numDistinct(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+        vector<vector<double>> dp(m+1,vector<double>(n+1,0));
+        for(int i=0;i<=m;i++)
+            dp[i][0] = 1;
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1] == t[j-1])
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return (int)dp[m][n];
     }
 };
