c722f4215e078f1e72edc9ccfe9b5abe950c3222
Sunny Siddhu <119279854+sunnysiddhu21@users.noreply.github.com>
2023-12-23 12:09:05 +0530

    Time: 7 ms (83.94%) | Memory: 9.3 MB (46.54%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int f(int i,int j,string &s, string &t , vector<vector<int>> &dp ){
-        if(i<0) return j+1;  //base case
-        if(j<0) return i+1;  //base case
+        if(i==0) return j;  //base case
+        if(j==0) return i;  //base case
         if(dp[i][j]!=-1) return dp[i][j];
 
-        if(s[i]==t[j]) return dp[i][j]=f(i-1,j-1,s,t,dp);
+        if(s[i-1]==t[j-1]) return dp[i][j]=f(i-1,j-1,s,t,dp);
 
         return dp[i][j]=1+min(f(i-1,j,s,t,dp),min(f(i,j-1,s,t,dp),f(i-1,j-1,s,t,dp)));
     }
@@ -13,7 +13,7 @@ public:
     int minDistance(string s, string t) {
         int n=s.size();
         int m=t.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,s,t,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return f(n,m,s,t,dp);
     }
 };
