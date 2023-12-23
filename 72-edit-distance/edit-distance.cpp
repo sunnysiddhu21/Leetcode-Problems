ce8d128eb9c0e55ef877f6ca813fbe81865f40a2
Sunny Siddhu <119279854+sunnysiddhu21@users.noreply.github.com>
2023-12-23 12:03:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (68.31%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,36 +1,19 @@
 class Solution {
 public:
-    int f(int i,int j,string &s, string &t ){
+    int f(int i,int j,string &s, string &t , vector<vector<int>> &dp ){
         if(i<0) return j+1;  //base case
         if(j<0) return i+1;  //base case
-        if(i=0) return j;  //base case
-        if(j=0) return i;
+        if(dp[i][j]!=-1) return dp[i][j];
 
-        if(s[i]==t[j]) return f(i-1,j-1,s,t);
+        if(s[i]==t[j]) return dp[i][j]=f(i-1,j-1,s,t,dp);
 
-        return 1+min(f(i-1,j,s,t),min(f(i,j-1,s,t),f(i-1,j-1,s,t)));
+        return dp[i][j]=1+min(f(i-1,j,s,t,dp),min(f(i,j-1,s,t,dp),f(i-1,j-1,s,t,dp)));
     }
 
     int minDistance(string s, string t) {
-        
         int n=s.size();
         int m=t.size();
-        vector<int> prev(m+1,0);
-        vector<int> curr(m+1,0);
-
-        for(int j =0; j<= m; j++){
-            prev[j] = j;
-        }
-
-        for(int i=1; i<=n; i++){
-            curr[0] = i;
-            for(int j=1; j<=m; j++){
-                if(s[i-1]==t[j-1]) curr[j]=prev[j-1];
-                else curr[j]=1+min(curr[j-1],min(prev[j-1],prev[j]));
-            }
-            prev=curr;
-        }
-
-        return prev[m];
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return f(n-1,m-1,s,t,dp);
     }
 };
