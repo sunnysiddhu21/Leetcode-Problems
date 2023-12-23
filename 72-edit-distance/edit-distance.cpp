9c2a04c9bad8f7e29a0fb24433955d9f3ad354c6
Sunny Siddhu <119279854+sunnysiddhu21@users.noreply.github.com>
2023-12-23 12:26:13 +0530

    Time: 13 ms (45.68%) | Memory: 9.5 MB (30.44%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -10,10 +10,19 @@ public:
         return dp[i][j]=1+min(f(i-1,j,s,t,dp),min(f(i,j-1,s,t,dp),f(i-1,j-1,s,t,dp)));
     }
 
-    int minDistance(string s, string t) {
-        int n=s.size();
-        int m=t.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return f(n,m,s,t,dp);
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        for(int j=0; j<=m; j++) dp[0][j] = j;
+        for (int i=0; i<=n; i++) dp[i][0] = i;
+
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=m; j++) {
+                if (word1[i-1] == word2[j-1]) dp[i][j] = 0 + dp[i-1][j-1];
+                else dp[i][j] = 1 + min(dp[i][j-1], min(dp[i-1][j], dp[i-1][j-1]));
+            }
+        }
+        return dp[n][m];
     }
 };
