92736ee087a58777630d7af4b2565ba9f5fd8f44
Sunny Siddhu <119279854+sunnysiddhu21@users.noreply.github.com>
2023-12-25 12:54:46 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (87.76%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        if(s.length()==0 || s[0]=='0') return 0;
+        if(s.length()==1) return 1;
+        int cnt1=1,cnt2=1;
+        for(int i=1; i<s.length(); i++){
+            int d=s[i]-'0';
+            int dd=(s[i-1]-'0')*10+d;
+            int cnt=0;
+            if(d>0) cnt+=cnt2;
+            if(dd>=10 && dd<=26) cnt+=cnt1;
+            cnt1=cnt2;
+            cnt2=cnt;
+        }
+        return cnt2;
+
+        
+    }
+};
