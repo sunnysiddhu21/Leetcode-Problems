1fe1f9faba6bbf51f303fe6caa369da9c237d940
Sunny Siddhu <119279854+sunnysiddhu21@users.noreply.github.com>
2023-12-25 13:48:58 +0530

    Time: 2 ms (57.79%) | Memory: 6.4 MB (31.03%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -4,15 +4,17 @@ public:
         
         dp[0]=1;
         dp[1]=1;
-        if(dp[n]!=-1)return dp[n];
-        int ans=0;
-        for(int i=1; i<=n; i++){
-            ans+=solve(i-1,dp)*solve(n-i,dp);
+        
+        
+        for(int i=2; i<=n; i++){
+            for(int j=1; j<=i; j++){
+              dp[i]+=dp[j-1]*dp[i-j];
+            }
         }
-        return dp[n]=ans;
+        return dp[n];
     }
     int numTrees(int n) {
-        vector<int> dp(n+1,-1);
+        vector<int> dp(n+1,0);
         return solve(n,dp);
     }
 };
