0378f5dddb0b407162e6d2d49e3e8ece1076e2ff
Sunny Siddhu <119279854+sunnysiddhu21@users.noreply.github.com>
2023-12-25 17:38:50 +0530

    Time: 3 ms (75.99%) | Memory: 7 MB (60.46%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-vector<vector<int>>memo;
-    bool helper(string&s1,string&s2,string&s3,int i,int j){
-        if(i+j==s3.length())return true;
-        if(memo[i][j]!=-1)return memo[i][j];
-        int ans=false;
-        if(i<s1.length()&&j<s2.length()&&s1[i]==s3[i+j]&&s2[j]==s3[i+j]){
-            bool take1 =helper(s1,s2,s3,i+1,j);
-            bool take2=helper(s1,s2,s3,i,j+1);
-            ans=take1||take2;
-        }
-        else if(i<s1.length()&&s1[i]==s3[i+j]){
-            ans=helper(s1,s2,s3,i+1,j);
-        }
-        else if(j<s2.length()&&s2[j]==s3[i+j]){
-            ans=helper(s1,s2,s3,i,j+1);
-        }
-        return memo[i][j]= ans;
 
-    }
     bool isInterleave(string s1, string s2, string s3) {
-        memo.resize(s1.length()+1,vector<int>(s2.length()+1,-1));
         if(s1.length()+s2.length()!=s3.length())return false;
-        return helper(s1,s2,s3,0,0);
+        vector<vector<bool>>dp(s1.length()+1,vector<bool>(s2.length()+1));
+        dp[0][0]=1; //satisfies because s1,s2,and s3 are empty strings
+        for(int j=1;j<s2.length()+1;j++){ //check when s1 length is zero
+             dp[0][j]=dp[0][j-1]&&(s2[j-1]==s3[j-1]);
+        }
+        for(int i=1;i<s1.length()+1;i++){ //check when s2 length is zero
+             dp[i][0]=dp[i-1][0]&&(s1[i-1]==s3[i-1]);
+        }
+        for(int i=1;i<s1.length()+1;i++){
+            for(int j=1;j<s2.length()+1;j++){
+                bool a=(s1[i-1]==s3[i+j-1])&&dp[i-1][j];
+                bool b=(s2[j-1]==s3[i+j-1])&&dp[i][j-1];
+                dp[i][j]=a||b;
+            }
+        }
+        return dp[s1.length()][s2.length()];
     }
 };
