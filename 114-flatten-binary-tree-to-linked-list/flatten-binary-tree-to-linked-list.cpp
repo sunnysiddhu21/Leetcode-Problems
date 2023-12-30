e55d9f5d0e499a24462341c092181211c7d1ebd9
Sunny Siddhu <119279854+sunnysiddhu21@users.noreply.github.com>
2023-12-31 00:18:16 +0530

    Time: 11 ms (8.42%) | Memory: 13.1 MB (43.91%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -11,27 +11,57 @@
  */
 class Solution {
 public:
-    
-    TreeNode* rightmost(TreeNode* root){
-        if (root->right==NULL) return root;
-        return rightmost(root->right);
-    }
-    
-    void flatten(TreeNode* root) {
-        if (root==NULL) return;
-        TreeNode* nextright;
-        TreeNode* rightMOST;
-        
-        while (root){
-            
-            if (root->left){
-                rightMOST = rightmost(root->left);
-                nextright = root->right;
-                root->right = root->left;
-                root->left=NULL;
-                rightMOST->right=nextright;
-            }
-            root=root->right;
+    TreeNode* flattenTree(TreeNode* root){
+        if(root == NULL){
+            return NULL;
         }
+        if(root->left == NULL && root->right == NULL){
+            return root;
+        }
+        
+        TreeNode* lastLeftPtr = flattenTree(root->left);
+        TreeNode* lastRightPtr = flattenTree(root->right);
+        if(lastLeftPtr != NULL){
+            if(root->right){
+                lastLeftPtr->right=root->right;
+            }
+            root->right = root->left;
+            root->left = NULL;
+        }
+            
+       
+        if(lastRightPtr == NULL){
+            return lastLeftPtr;
+        }
+        return lastRightPtr;
+    }
+
+    void printTree(TreeNode* root){
+        if(root==NULL){
+            return;
+        }
+        cout << "root->val:  " << root->val << endl;
+        if(root->left){
+            cout << "root->left->val:  " << root->left->val<<  endl;
+        }
+        if(root->right){
+            cout << "root->right->val:  " << root->right->val<<  endl;
+        }
+        printTree(root->left);
+        printTree(root->right);
+    }
+
+
+    void flatten(TreeNode* root) {
+        cout << "*********************" << endl;
+        if(root == NULL){
+            return;
+        }
+        flattenTree(root);
+        if(root->left == NULL){
+            flattenTree(root->right);
+        }
+        cout << "Printing Tree" << endl;
+        printTree(root);
     }
 };
