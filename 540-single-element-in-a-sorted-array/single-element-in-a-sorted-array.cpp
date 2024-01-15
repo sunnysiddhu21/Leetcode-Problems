4686e88b3bf4d30821eaeba7a15f2a71c73e5685
Sunny Siddhu <119279854+sunnysiddhu21@users.noreply.github.com>
2024-01-15 16:14:52 +0530

    Time: 27 ms (19.10%) | Memory: 22.9 MB (12.14%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -3,22 +3,20 @@ public:
     int singleNonDuplicate(vector<int>& arr) {
         int n = arr.size(); //size of the array.
         if (n == 1) return arr[0];
+        if(arr[0]!=arr[1]) return arr[0];
+        if(arr[n-1]!=arr[n-2]) return arr[n-1];
 
-        for (int i = 0; i < n; i++) {
-
-            //Check for first index:
-            if (i == 0) {
-                if (arr[i] != arr[i + 1])
-                    return arr[i];
+        int l=1;
+        int h=n-2;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(arr[mid]!=arr[mid-1] && arr[mid]!=arr[mid+1]){
+                return arr[mid];
             }
-            //Check for last index:
-            else if (i == n - 1) {
-                if (arr[i] != arr[i - 1])
-                    return arr[i];
-            }
-            else {
-                if (arr[i] != arr[i - 1] && arr[i] != arr[i + 1])
-                    return arr[i];
+            if((mid%2==1 && arr[mid]==arr[mid-1]) || (mid%2==0 && arr[mid]==arr[mid+1])){
+                l=mid+1;
+            }else{
+                h=mid-1;
             }
         }
 
