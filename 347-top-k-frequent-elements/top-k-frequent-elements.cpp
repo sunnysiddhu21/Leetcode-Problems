3aa215617d1239ad375fd3ef2fe6af397821d7df
Sunny Siddhu <119279854+sunnysiddhu21@users.noreply.github.com>
2024-02-03 15:47:15 +0530

    Time: 16 ms (35.45%) | Memory: 17 MB (19.23%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    static bool cmp(pair<int, int>& a, pair<int, int>& b) {
+        return a.second > b.second;
+    }
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        // Step 1: Use an unordered_map (mp) to count the frequency of each element in nums.
+        unordered_map<int, int> mp;
+        for (auto x : nums) {
+            mp[x]++;
+        }
+
+        // Step 2: Create a vector of pairs (v) to store the elements and their frequencies.
+        vector<pair<int, int>> v;
+        for (auto x : mp) {
+            v.push_back(pair{x.first, x.second});
+        }
+
+        // Step 3: Sort the vector based on frequencies in descending order.
+        sort(v.begin(), v.end(), cmp);
+
+        // Step 4: Create a vector (ans) to store the top k frequent elements.
+        vector<int> ans;
+        for (int i = 0; i < k; i++) {
+            auto it = v.begin() + i;
+            ans.push_back(it->first);
+        }
+        return ans;
+    }
+};
+        
