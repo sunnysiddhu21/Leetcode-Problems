c616286c3f13f77f95f522a88dc758b4bfb35aff
Sunny Siddhu <119279854+sunnysiddhu21@users.noreply.github.com>
2024-02-03 14:59:53 +0530

    Time: 4 ms (86.02%) | Memory: 8.5 MB (10.59%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()) return false;
+        
+        int count[26] = {0};
+        
+        // Count the frequency of characters in string s
+        for (char x : s) {
+            count[x - 'a']++;
+        }
+        
+        // Decrement the frequency of characters in string t
+        for (char x : t) {
+            count[x - 'a']--;
+        }
+        
+        // Check if any character has non-zero frequency
+        for (int val : count) {
+            if (val != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+
+    }
+};
