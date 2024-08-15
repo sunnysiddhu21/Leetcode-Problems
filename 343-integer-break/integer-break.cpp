a644b57e4149297d6f865d9ba4fe43f8984165a0
Sunny Siddhu <119279854+sunnysiddhu21@users.noreply.github.com>
2024-08-15 12:37:34 +0530

    Time: 3 ms (36.45%) | Memory: 7.7 MB (32.36%) - LeetSync

--- 343-integer-break/integer-break.cpp
+++ 343-integer-break/integer-break.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
     
-    int integerBreakUtil(int ind,vector<int>&v,int sum,int &k,vector<vector<int>>&dp){
-        int n=v.size();
+    int integerBreakUtil(int n,vector<int>&dp){
+        if(n==0) return 0;
+        int result=INT_MIN;
+        if(dp[n]!=-1) return dp[n];
 
-        if(ind==n || sum==n){
-            if(k>=2) return 1;
-            return 0;
+        for(int i=1; i<=n-1; i++){
+            int prod=i*max(n-i,integerBreakUtil(n-i,dp));
+            result=max(result,prod);
         }
 
-        if(dp[ind][sum]!=-1) return dp[ind][sum];
-
-        int ntake=integerBreakUtil(ind+1,v,sum,k,dp);
-        int take=1;
-        if(sum+v[ind]<=n){
-            k++;
-            take=v[ind]*integerBreakUtil(ind,v,sum+v[ind],k,dp);
-        }
-
-        return dp[ind][sum]=max(take,ntake);
+        return dp[n]=result;
     }
 
     int integerBreak(int n) {
-        if(n==1) return 1;
-        if(n==0) return 0;
-        
-        vector<int> v;
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        for(int i=1; i<=n; i++){
-            v.push_back(i);
-        }
-
-        int k=0;
-
-        return integerBreakUtil(0,v,0,k,dp);
+        vector<int> dp(n+1,-1);
+        return integerBreakUtil(n,dp);
     }
 };
